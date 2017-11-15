7cbc00e10eb39d55af8293c52965358bf0eec091
kapoiosAllos <kyriakos.z@yahoo.com>
2017-11-15 12:27:19 +0100

    Profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  
-devise_for :users
-  
-  resources :products, only: [:index, :show]
 
-  
+devise_for :users
+
+  resources :products, only: [:index, :show]
+  resoures :profiles
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
